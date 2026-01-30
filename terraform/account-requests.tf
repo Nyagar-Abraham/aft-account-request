0ff9693fab3837e058c7ae9322444f062916413d
Nyagar-Abraham <abrahamowino129@gmail.com>
2026-01-30 09:40:12 +0300

    first account request

---
+++ terraform/account-requests.tf
@@ -0,0 +1,35 @@
+module "account_request_01" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "owinonyagarabraham@gmail.com"
+    AccountName               = "Vendor-account-1"
+    ManagedOrganizationalUnit = "Infrastructure (ou-15wl-64pnmhtw)" 
+    SSOUserEmail              = "owinonyagarabraham@gmail.com"
+    SSOUserFirstName          = "Owino"
+    SSOUserLastName           = "Nyagar"
+  }
+
+  account_tags = {
+    "ABC:Owner"       = "owinonyagarabraham@gmail.com"
+    "ABC:Division"    = "ENT"
+    "ABC:Environment" = "Dev"
+    "ABC:CostCenter"  = "123456"
+    "ABC:Vended"      = "true"
+    "ABC:DivCode"     = "102"
+    "ABC:BUCode"      = "ABC003"
+    "ABC:Project"     = "123456"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "AWS Control Tower Lab"
+    change_reason       = "Learn AWS Control Tower Account Factory for Terraform (AFT)"
+  }
+
+  custom_fields = {
+    custom1 = "a"
+    custom2 = "b"
+  }
+
+  account_customizations_name = "SANDBOX"
+}
